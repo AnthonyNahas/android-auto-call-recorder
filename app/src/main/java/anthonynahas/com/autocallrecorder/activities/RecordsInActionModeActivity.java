b36f499c62bb3a2d2111a28d82407b6eaaa6af84
Anthony Nahas <anthony.na@hotmail.de>
2017-04-28 16:16:23 +0200

    Ref: addd new activity

---
+++ app/src/main/java/anthonynahas/com/autocallrecorder/activities/RecordsInActionModeActivity.java
@@ -0,0 +1,15 @@
+package anthonynahas.com.autocallrecorder.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import anthonynahas.com.autocallrecorder.R;
+
+public class RecordsInActionModeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_records_in_action_mode);
+    }
+}
